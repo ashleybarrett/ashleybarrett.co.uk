339ca65369e8be66bff25e2da25400d43a54bff4
Ashley Barrett <ashleybarrett.mail@googlemail.com>
2018-01-06 18:49:38 +0000

    small rework

--- src/web/Startup.cs
+++ src/web/Startup.cs
@@ -7,7 +7,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
 
-namespace website
+namespace web
 {
     public class Startup
     {
@@ -15,18 +15,13 @@ namespace website
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddMvc();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
-            if (env.IsDevelopment())
-            {
-                app.UseDeveloperExceptionPage();
-            }
-
-            app.UseDefaultFiles();
-            app.UseStaticFiles();
+            app.UseMvc();
         }
     }
 }
